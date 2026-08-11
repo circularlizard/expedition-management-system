bb778775fff29c867d8361dd1a1399e2816039a0
David Strachan <davidst@hcl.com>
2026-08-11 11:16:38 -0400

    chore(version): update plugin version to 0.1.36 and refresh build timestamp

--- ems-plugin.php
+++ ems-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Expedition Management System (EMS)
  * Description: Manages DofE expeditions, teams, and route planning.
- * Version: 0.1.34
+ * Version: 0.1.36
  * Author: SE Scotland DofE (David Strachan)
  * Text Domain: ems-plugin
  * Requires PHP: 8.2
